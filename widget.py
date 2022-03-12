b8e8eaca26386027d0c8261b7768ddd3bff7f7c8
stensilart <stensilart@gmail.com>
2022-03-12 18:19:37 +0300

    tree

--- widget.py
+++ widget.py
@@ -4,101 +4,85 @@ app = QtWidgets.QApplication([])
 ui = uic.loadUi("form.ui")
 ui.setWindowTitle("KalGUI")
 
-
 a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-chislo1 =""
-chislo2 =""
+chislo = ['', '', '', '']
 ravno = ""
-glob1 = ""
-glob2 = ""
-glob3 = ""
+glob = ['', '', '', '']
 znak = ""
 
 def button(vvod):
-    global glob1
-    global glob2
-    global glob3
+    global glob
     global a
 
     vvod = int(vvod)
 
-    ui.line.setText(glob1 + str(vvod))
+    ui.line.setText(glob[1] + str(vvod))
     a[vvod] = ui.line.text()
 
-    ui.label.setText(glob2 + str(vvod))
-    glob2 = ui.label.text()
+    ui.label.setText(glob[2] + str(vvod))
+    glob[2] = ui.label.text()
 
     print(a[vvod])
-    print(glob2)
+    print(glob[2])
 
-    glob1 = a[vvod]
+    glob[1] = a[vvod]
 
-def znak (znak_vvod):
-    global glob1
-    global glob2
-    global chislo1
-    global chislo2
+def znak_f (znak_vvod):
+
+    global glob
+    global chislo
     global znak
 
-
-    chislo1 = glob1
-    glob1 = ""
+    chislo[1] = glob[1]
+    glob[1] = ""
     ui.line.setText(znak_vvod)
     znak = znak_vvod
 
-    ui.label.setText(glob2 + znak_vvod)
-    glob2 = ui.label.text()
+    ui.label.setText(glob[2] + znak_vvod)
+    glob[2] = ui.label.text()
 
     print(znak)
 
-
 def ravno():
-    global chislo1
-    global chislo2
+    global chislo
     global ravno
     global znak
 
-    chislo2 = glob1
+    chislo[2] = glob[1]
 
     if znak == "+":
-        ravno = int(chislo1) + int(chislo2)
+        ravno = int(chislo[1]) + int(chislo[2])
         ravno = str(ravno)
         ui.line.setText(ravno)
         print(ravno)
+
     if znak == "-":
-        ravno = int(chislo1) - int(chislo2)
+        ravno = int(chislo[1]) - int(chislo[2])
         ravno = str(ravno)
         ui.line.setText(ravno)
         print(ravno)
 
     if znak == "/":
-        ravno = int(chislo1) / int(chislo2)
+        ravno = int(chislo[1]) / int(chislo[2])
         ravno = str(ravno)
         ui.line.setText(ravno)
         print(ravno)
 
     if znak == "*":
-        ravno = int(chislo1) * int(chislo2)
+        ravno = int(chislo[1]) * int(chislo[2])
         ravno = str(ravno)
         ui.line.setText(ravno)
         print(ravno)
 
 def stiranie():
-    global chislo1
-    global chislo2
-    global glob1
-    global glob2
-    global g_odin
-    global g_dva
+    global chislo
+    global glob
     global znak
 
-    chislo1 = ""
-    chislo2 = ""
-    glob1 = ""
-    glob2 = ""
-    g_odin = ""
-    g_dva = ""
+    chislo[1] = ""
+    chislo[2] = ""
+    glob[1] = ""
+    glob[2] = ""
     znak = ""
     ui.line.setText("0")
     ui.label.setText("0")
@@ -113,10 +97,10 @@ ui.b_6.clicked.connect(lambda: button(6))
 ui.b_7.clicked.connect(lambda: button(7))
 ui.b_8.clicked.connect(lambda: button(8))
 ui.b_9.clicked.connect(lambda: button(9))
-ui.b_p.clicked.connect(lambda: znak("+"))
-ui.b_m.clicked.connect(lambda: znak("-"))
-ui.b_u.clicked.connect(lambda: znak("*"))
-ui.b_d.clicked.connect(lambda: znak("/"))
+ui.b_p.clicked.connect(lambda: znak_f("+"))
+ui.b_m.clicked.connect(lambda: znak_f("-"))
+ui.b_u.clicked.connect(lambda: znak_f("*"))
+ui.b_d.clicked.connect(lambda: znak_f("/"))
 ui.b_r.clicked.connect(ravno)
 ui.b_ce.clicked.connect(stiranie)
 
